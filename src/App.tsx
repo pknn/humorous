1f9f0f61ab461e9e0a841ce759f0462cbf7d1644
Pakanon Pantisawat <pakanon.pantisawat@gmail.com>
2022-04-22 13:38:29 +0700

    feat: added ColorBox component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { ColorBox } from './components/ColorBox'
 import { Header } from './components/Header'
 
 const App = () => (
   <div className="p-2 bg-neutral-200 h-screen w-screen">
     <Header />
+    <ColorBox color={{ type: 'HSL', components: ['12', '20%', '60%'] }} />
+    <ColorBox color={{ type: 'RGB', components: ['233', '245', '182'] }} />
   </div>
 )
 
---
+++ src/components/ColorBox.tsx
@@ -0,0 +1,17 @@
+import { useMemo } from 'react'
+import { Color } from '../model/color'
+
+export const getColorProperty = ({ type, components }: Color) =>
+  `${type.toLowerCase()}(${components.join(',')})`
+
+interface ColorBoxProps {
+  color: Color
+}
+
+export const ColorBox = ({ color }: ColorBoxProps) => {
+  const colorProperty = useMemo(() => getColorProperty(color), [color])
+
+  return (
+    <div style={{ background: colorProperty }} className="w-full h-full"></div>
+  )
+}
