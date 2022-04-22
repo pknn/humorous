62c13c82664e3861d316a4fdb77e8e1be9767f1b
Pakanon Pantisawat <pakanon.pantisawat@gmail.com>
2022-04-22 14:49:41 +0700

    feat: extracted colors to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Header } from './components/Header/Header'
 import { ColorBoxes } from './components/ColorBoxes'
+import { useColors } from './hooks/useColors/useColors'
 
-const App = () => (
-  <div className="p-2 bg-neutral-200 h-screen flex flex-col">
-    <Header />
-    <ColorBoxes />
-  </div>
-)
+const App = () => {
+  const [colors, status, refresh] = useColors()
+
+  return (
+    <div className="p-2 bg-neutral-200 h-screen flex flex-col">
+      <Header />
+      <ColorBoxes colors={colors} />
+    </div>
+  )
+}
 
 export default App
--- src/components/ColorBoxes/ColorBoxes.tsx
+++ src/components/ColorBoxes/ColorBoxes.tsx
@@ -2,20 +2,14 @@ import { Color } from '../../model/color'
 import { ColorBox } from '../ColorBox/ColorBox'
 import { Container } from './ColorBoxes.tw'
 
-export const ColorBoxes = () => {
-  const colors: Color[] = [
-    { type: 'HSL', components: ['12', '20%', '60%'] },
-    { type: 'RGB', components: ['233', '245', '182'] },
-    { type: 'HSL', components: ['12', '20%', '60%'] },
-    { type: 'RGB', components: ['233', '245', '182'] },
-    { type: 'HSL', components: ['12', '20%', '60%'] },
-  ]
-
-  return (
-    <Container>
-      {colors.map((color) => (
-        <ColorBox color={color} key={JSON.stringify(color)} />
-      ))}
-    </Container>
-  )
+interface Props {
+  colors: Color[]
 }
+
+export const ColorBoxes = ({ colors }: Props) => (
+  <Container>
+    {colors.map((color) => (
+      <ColorBox color={color} key={JSON.stringify(color)} />
+    ))}
+  </Container>
+)
