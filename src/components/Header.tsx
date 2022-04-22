bcdea49e9a5325dd8d9b6612b65731dba7ef2bd2
Pakanon Pantisawat <pakanon.pantisawat@gmail.com>
2022-04-22 12:58:44 +0700

    style: added styling to Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,12 @@
 export const Header = () => (
-  <div className="text-center mt-4">
-    <h1 className="text-5xl font-play">Humorous</h1>
-    <h2>The only one color generator you will ever need</h2>
+  <div className="text-center mt-8 md:mt-6">
+    <div>
+      <span className="text-5xl md:text-6xl font-play bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent">
+        Humorous
+      </span>
+    </div>
+    <h2 className="text-sm md:text-md">
+      The only one color generator you will ever need
+    </h2>
   </div>
 )
